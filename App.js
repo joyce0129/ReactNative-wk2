86058a831868920b93514c5d2891ecb7a551b813
joyce0129 <131430612+joyce0129@users.noreply.github.com>
2024-03-09 13:55:17 +0800

    Update App.js

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { SafeAreaView,StatusBar } from "react-native";
 import Header from "./components/Header";
 import AlbumList from "./components/Albumlist";
+import albumData from "./components/albums.json";
 
 
-const App=()=>{
-  return(
-    <SafeAreaView style={{flex:1}}>
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
       <StatusBar />
-      <Header />
+      <Header title={albumData.albumTitle} />
       <AlbumList />
     </SafeAreaView>
   );
