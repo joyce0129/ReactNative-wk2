53cf095843ae2e411237c0da80c0636df56ee854
joyce0129 <131430612+joyce0129@users.noreply.github.com>
2024-03-09 13:54:46 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,30 +1,29 @@
 import React from "react";
 import { StyleSheet,Text,View } from "react-native";
 
-const Header = ()=> {
-    return(
-        <View style={styles.headerstyle}>
-            <Text style={styles.textStyle}>Albums</Text>
+const Header = ({ title }) => {
+    return (
+        <View style={styles.headerStyle}>
+          <Text style={styles.textStyle}>{title}</Text>
         </View>
     );
-};
-
-const styles = StyleSheet.create({
-    headerstyle:{
-        backgroundColor:'#D9D9D9',
-        justifyContent:'center',
-        alignItems:'center',
-        height:90,
-        paddingTop:30,
-        shadowColor:'#000',
-        shadowOffset:{ width:0,height:2},
-        shadowOpacity:0.2,
-        elevation:2
-
+  };
+  
+  const styles = StyleSheet.create({
+    headerStyle: {
+      backgroundColor: "#F8F8F8",
+      justifyContent: "center",
+      alignItems: "center",
+      height: 60,
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 2 },
+      shadowOpacity: 0.2,
+      // Android Only
+      elevation: 2
     },
-    textStyle:{
-        fontSize:30
+    textStyle: {
+      fontSize: 20
     },
-});
-
-export default Header;
+  });
+  
+  export default Header;
